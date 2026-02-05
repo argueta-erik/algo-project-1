5310c949886cea351f994e071a5530c9110164c9
Erik Argueta <argueta.erik@csu.fullerton.edu>
2026-02-05 12:28:47 -0800

    updated requirements and main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time     # import time module to measure duration functions
-import random   # import random module to genarate random input data for the algorithms
+from numpy import random   # import random module to genarate random input data for the algorithms
 
 # ===============
 # Time Functions
@@ -9,6 +9,13 @@ import random   # import random module to genarate random input data for the alg
 # ============
 # Bubble Sort
 # ============
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j+1] = arr[j + 1], arr[j]
+    return arr
 
 # ============
 # Merge Sort
@@ -30,4 +37,9 @@ import random   # import random module to genarate random input data for the alg
 
 # main
 if __name__ == "__main__":
-    print("Hello World")
+    test_arr = [10, 8, 9, 1, 6, 3, 7]
+    test_ran_arr = random.randint(10, size=(7))
+
+    print("Unsorted Array:", test_arr)
+    print ("Unsorted Random Array:", test_ran_arr)
+    print("Bubble Sort:", bubble_sort(test_ran_arr))
