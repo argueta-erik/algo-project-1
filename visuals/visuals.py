e705d22bc87700690262e9e138d011d853b18cfd
Erik Argueta <argueta.erik@csu.fullerton.edu>
2026-02-12 22:58:01 -0800

    Updated Requirements.txt and added rectangle to visuals.py

--- visuals/visuals.py
+++ visuals/visuals.py
@@ -1,4 +1,4 @@
-# Example file showing a circle moving on screen
+# Example file showing a basic pygame "game loop"
 import pygame
 
 # pygame setup
@@ -6,9 +6,6 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
-dt = 0
-
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
     # poll for events
@@ -18,28 +15,14 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white")
+    screen.fill("gray")
 
-    pygame.draw.rect(screen, "green", )
-
-    # pygame.draw.circle(screen, "red", player_pos, 40)
-
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_w]:
-    #     player_pos.y -= 300 * dt
-    # if keys[pygame.K_s]:
-    #     player_pos.y += 300 * dt
-    # if keys[pygame.K_a]:
-    #     player_pos.x -= 300 * dt
-    # if keys[pygame.K_d]:
-    #     player_pos.x += 300 * dt
+    # RENDER YOUR GAME HERE
+    pygame.draw.rect(screen, "black", (300, 100, 200, 100), 3)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+    clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
